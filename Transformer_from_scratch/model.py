6770c9f47d1052a608702b78f4322d013bfb9ef0
eyebrowKIM <albert1226@g.skku.edu>
2024-05-25 16:07:04 +0900

    1st update

---
+++ Transformer_from_scratch/model.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+import math
+
+class InputEmbeddings(nn.Module):
+    
+    def __init__(self, d_model: int, vocab_size: int):
+        super().__init__()
+        self.d_model = d_model
+        self.vocab_size = vocab_size
+        self.embedding = nn.Embedding(vocab_size, d_model)
+    
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+    
+    def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(p=dropout)
+        
+        # Create a matrix of shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        # Apply the sin to even positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        
+        self.pe = pe.unsqueeze(0) # (1, Seq_len, d_model)
+        
+        self.register_buffer('pe', pe)
+        
+    def forward(self, x):
+        x += self.pe[:, :x.shape[1], :].requires_grad_(False)
+        return self.dropout(x)
+    
+class LayerNormalization(nn.Module):
+    
+    def __init__(self, eps: float = 10**-6) -> None:
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.tensor(1.0)) # Multiplied
+        self.bias = nn.Parameter(torch.tensor(0.0)) # Added
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(dim=-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+    
+    
+class FeedForwardBlock(nn.Module):
+    
+    def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+        super.__init__()
+        self.linear_1 = nn.Linear(d_model, d_ff)
+        self.dropout = nn.Dropout(dropout)
+        self.linear_2 = nn.Linear(d_ff, d_model)
+        
+    def forward(self, x):
+        # (Batch, Seq_Len, d_model) -> (Batch, Seq_Len, d_ff) -> (Batch, Seq_Len, d_model)
+        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+class MultiHeadAttention(nn.Module):
+    
+    def __init__(self, d_model: int, h: int, dropout: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model must be divisible by h"
+        
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        
+        self.w_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        
+    def forward(self, q: torch.tensor, k: torch.tensor, v: torch.tensor, mask):
+        # (Batch, Seq_Len, d_model) -> (Batch, Seq_Len, d_model)
+        query, key, value = self.w_q(q), self.w_k(k), self.w_v(v) 
+        
+        
